68f2553aafb971dc7076b0cb06267b989955925c
Tim Rogers <tim@gocardless.com>
2017-05-30 22:31:41 +0100

    Add test for filtering the transactions API

--- spec/integration/transactions_spec.rb
+++ spec/integration/transactions_spec.rb
@@ -13,36 +13,60 @@ RSpec.describe Starling::Services::TransactionsService do
   describe '#list' do
     subject { service.list }
 
-    before do
-      stub_request(:get, 'https://api.starlingbank.com/api/v1/transactions')
-        .with(headers: {
-                'Accept' => 'application/json',
-                'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-                'Authorization' => 'Bearer dummy_access_token',
-                'User-Agent' => 'starling-ruby/v0.1.0 ruby/2.4.1p111 ruby/2.4.1 ' \
-                          'x86_64-darwin16 faraday/0.9.2'
-              })
-        .to_return(status: status,
-                   body: body,
-                   headers: headers)
-    end
-
     let(:status) { 200 }
     let(:body) { load_fixture('transactions.json') }
     let(:headers) { { 'Content-Type' => 'application/json' } }
 
-    its(:length) { is_expected.to eq(12) }
+    context 'with no filters' do
+      before do
+        stub_request(:get, 'https://api.starlingbank.com/api/v1/transactions')
+          .with(headers: {
+                  'Accept' => 'application/json',
+                  'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                  'Authorization' => 'Bearer dummy_access_token',
+                  'User-Agent' => 'starling-ruby/v0.1.0 ruby/2.4.1p111 ruby/2.4.1 ' \
+                            'x86_64-darwin16 faraday/0.9.2'
+                })
+          .to_return(status: status,
+                     body: body,
+                     headers: headers)
+      end
 
-    describe '#first' do
-      subject(:transaction) { service.list.first }
+      its(:length) { is_expected.to eq(12) }
 
-      its(:created) { is_expected.to eq(Time.parse('2017-05-30T18:06:28.773Z')) }
-      its(:id) { is_expected.to eq('ed973200-037d-4f55-9c67-1b467a341203') }
-      its(:currency) { is_expected.to eq('GBP') }
-      its(:amount) { is_expected.to eq(-1.02) }
-      its(:direction) { is_expected.to eq(:outbound) }
-      its(:narrative) { is_expected.to eq('Aldi') }
-      its(:source) { is_expected.to eq(:master_card) }
+      describe '#first' do
+        subject(:transaction) { service.list.first }
+
+        its(:created) { is_expected.to eq(Time.parse('2017-05-30T18:06:28.773Z')) }
+        its(:id) { is_expected.to eq('ed973200-037d-4f55-9c67-1b467a341203') }
+        its(:currency) { is_expected.to eq('GBP') }
+        its(:amount) { is_expected.to eq(-1.02) }
+        its(:direction) { is_expected.to eq(:outbound) }
+        its(:narrative) { is_expected.to eq('Aldi') }
+        its(:source) { is_expected.to eq(:master_card) }
+      end
+    end
+
+    context 'filtering by date' do
+      let!(:stub) do
+        stub_request(:get,
+                     'https://api.starlingbank.com/api/v1/transactions?from=2017-05-30')
+          .with(headers: {
+                  'Accept' => 'application/json',
+                  'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                  'Authorization' => 'Bearer dummy_access_token',
+                  'User-Agent' => 'starling-ruby/v0.1.0 ruby/2.4.1p111 ruby/2.4.1 ' \
+                            'x86_64-darwin16 faraday/0.9.2'
+                })
+          .to_return(status: status,
+                     body: body,
+                     headers: headers)
+      end
+
+      it 'adds the provided filter to the URL parameters' do
+        service.list(params: { from: '2017-05-30' })
+        expect(stub).to have_been_requested
+      end
     end
   end
 
